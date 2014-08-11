5f5fc0b80f03469cd4b7cc3506e894bdbde286a1
Olivier Samyn <code@oleastre.be>
2014-08-12 01:14:23 +0200

    Started writing rune text depending on player yaw.
    This needs some adjustments:
    - correct orientation detection (current text is wrong)
    - use rune yaw instead of player one (static text).

--- runes.lua
+++ runes.lua
@@ -26,7 +26,25 @@ for i, p in ipairs(rune.pick_types) do
   })
 end
 
+function rune.get_quad(player)
+  local pi2 = math.pi*2
+  local pi4 = math.pi/4
+  
+  local dir = player:get_look_yaw()
+  while (dir<0) do dir = dir + pi2 end
+  while (dir>pi2) do dir = dir - pi2 end
+  
+  local quad = 0
+  if(dir>pi4) then quad=1 end
+  if(dir>3*pi4) then quad=2 end
+  if(dir>5*pi4) then quad=3 end
+  if(dir>7*pi4) then quad=0 end
+  return quad
+end
+
 function rune.handle_rightclick(pos, node, clicker)
+  minetest.debug("look at quad="..rune.get_quad(clicker))
+    
   local meta = minetest.get_meta(pos)
   local rune_type_idx = meta:get_int("rune:type") + 1
   if rune_type_idx > #rune.types then
@@ -37,10 +55,47 @@ function rune.handle_rightclick(pos, node, clicker)
 
   local res = minetest.find_node_near(pos, rune.search_size, "default:stone_with_"..rune_type)
   if res then
+    local wy, wy, wz
     local dx = math.floor(res.x - pos.x)
     local dy = math.floor(res.y - pos.y)
     local dz = math.floor(res.z - pos.z)
-    meta:set_string("infotext", "Found "..rune_type.." at ("..dx..","..dy..","..dz..") from your current position.")
+    
+    if (dy > 0) then 
+      wy = ", "..dy.." above"
+    elseif (dy==0) then 
+      wy = ""
+    else 
+      wy = ", "..(-dy).." below"
+    end
+    
+    if quad==2 then 
+      dx = -dx
+      dz = -dz
+    elseif quad==1 then
+      local t = dx
+      dx = dz
+      dz = -t
+    elseif quad==3 then
+      local t = dx
+      dx = -dz
+      dz = t
+    end
+    
+    if (dx >= 0) then 
+      wx =  dx.." steps forward"
+    else 
+      wx =  (-dx).." steps backward"
+    end
+    
+    if (dz > 0) then 
+      wz = ", "..dz.." left"
+    elseif (dz==0) then
+      wz= ""
+    else
+      wz =", "..(-dz).." right"
+    end
+    
+    meta:set_string("infotext", "Found "..rune_type.." "..wx..wz..wy)
   else
     meta:set_string("infotext", "No "..rune_type.." found in the surrounding area.")
   end
