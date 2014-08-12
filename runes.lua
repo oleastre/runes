b8a6c55710a0df1faf65fc7f504ff7e7615736be
Olivier Samyn <olivier@26lights.com>
2014-08-12 14:23:10 +0200

    Create text based on the rune orientation; add punch to reset text.

--- runes.lua
+++ runes.lua
@@ -14,7 +14,12 @@ rune.pick_types = {
 rune.types = {
   "coal",
   "iron",
-  "tin"
+  "tin",
+  "copper",
+  "silver",
+  "gold",
+  "diamond",
+  "mese"
 }
 
 rune.search_size = 7
@@ -26,33 +31,10 @@ for i, p in ipairs(rune.pick_types) do
   })
 end
 
-function rune.get_quad(player)
-  local pi2 = math.pi*2
-  local pi4 = math.pi/4
-  
-  local dir = player:get_look_yaw()
-  while (dir<0) do dir = dir + pi2 end
-  while (dir>pi2) do dir = dir - pi2 end
-  
-  local quad = 0
-  if(dir>pi4) then quad=1 end
-  if(dir>3*pi4) then quad=2 end
-  if(dir>5*pi4) then quad=3 end
-  if(dir>7*pi4) then quad=0 end
-  return quad
-end
-
-function rune.handle_rightclick(pos, node, clicker)
-  minetest.debug("look at quad="..rune.get_quad(clicker))
-    
+function rune.search_ore(pos, node)
   local meta = minetest.get_meta(pos)
-  local rune_type_idx = meta:get_int("rune:type") + 1
-  if rune_type_idx > #rune.types then
-    rune_type_idx = 1
-  end
+  local rune_type_idx = meta:get_int("rune:type")
   local rune_type = rune.types[rune_type_idx]
-  minetest.swap_node(pos, {name="oleastre:rune_"..rune_type, param1=node.param1, param2=node.param2 })
-
   local res = minetest.find_node_near(pos, rune.search_size, "default:stone_with_"..rune_type)
   if res then
     local wy, wy, wz
@@ -68,39 +50,53 @@ function rune.handle_rightclick(pos, node, clicker)
       wy = ", "..(-dy).." below"
     end
     
-    if quad==2 then 
+    local dir = node.param2
+    --minetest.debug("param2="..dir.." dx="..dx.." dy="..dy.." dz="..dz)
+    
+    if (dir==1) or (dir==2) then 
       dx = -dx
       dz = -dz
-    elseif quad==1 then
-      local t = dx
-      dx = dz
-      dz = -t
-    elseif quad==3 then
+    elseif dir==4 then
       local t = dx
       dx = -dz
       dz = t
+    elseif dir==5 then
+      local t = dx
+      dx = -dz
+      dz = -t
     end
     
-    if (dx >= 0) then 
-      wx =  dx.." steps forward"
-    else 
-      wx =  (-dx).." steps backward"
+    if (dx > 0) then 
+      wx = dx.." steps backward"
+    else
+      wx = (-dx).." steps forward"
     end
     
     if (dz > 0) then 
-      wz = ", "..dz.." left"
+      wz = ", "..dz.." right"
     elseif (dz==0) then
       wz= ""
     else
-      wz =", "..(-dz).." right"
+      wz =", "..(-dz).." left"
     end
     
     meta:set_string("infotext", "Found "..rune_type.." "..wx..wz..wy)
   else
     meta:set_string("infotext", "No "..rune_type.." found in the surrounding area.")
   end
+end
 
+
+function rune.handle_rightclick(pos, node, clicker)
+  local meta = minetest.get_meta(pos)
+  local rune_type_idx = meta:get_int("rune:type") + 1
+  if rune_type_idx > #rune.types then
+    rune_type_idx = 1
+  end
+  local rune_type = rune.types[rune_type_idx]
+  minetest.swap_node(pos, {name="oleastre:rune_"..rune_type, param1=node.param1, param2=node.param2 })
   meta:set_int("rune:type", rune_type_idx)
+  rune.search_ore(pos, node)
 end
 
 minetest.register_node("oleastre:rune", {
@@ -139,6 +135,9 @@ for i, t in ipairs(rune.types) do
     groups = {choppy = 2, dig_immediate = 2},
     on_rightclick = function(pos, node, clicker)
       rune.handle_rightclick(pos, node, clicker)
+    end,
+    on_punch = function(pos, node, clicker)
+      rune.search_ore(pos, node)
     end,
   })
 end
