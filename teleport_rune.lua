91ed4d971322522eff52da458c2eb5a333d188f9
Olivier Samyn 🎻 <code@oleastre.be>
2020-06-17 18:53:55 +0200

    Add recipe to create stones with memory and save/restore meta data logic.

--- teleport_rune.lua
+++ teleport_rune.lua
@@ -20,13 +20,35 @@ teleport_rune.create_gravel = function(itemstack, user, pointed_thing)
     return itemstack
 end
 
-teleport_rune.set_gravel_text = function(pos, text)
+teleport_rune.set_block_text = function(pos, text)
     if not text then
         text = S("At position: @1, @2, @3", pos.x, pos.y, pos.z)
     end
     local meta = minetest.get_meta(pos)
     meta:set_string("text", text)
     meta:set_string("infotext", text)
+end
+
+teleport_rune.concat_description = function(description, text)
+    local suffix = ""
+    if text~=nil then
+        suffix = " - " .. text
+    end
+    return S(description) .. suffix
+end
+
+teleport_rune.save_block_meta = function(pos, description, block_meta, item_meta)
+    item_meta:set_string("text", block_meta["text"])
+    item_meta:set_string("description", teleport_rune.concat_description(description, block_meta["text"]))
+    item_meta:set_string("rune_pos", block_meta["rune_pos"] or minetest.serialize(pos))
+    return item_meta
+end
+
+teleport_rune.restore_block_meta = function(item_meta, block_meta)
+    block_meta:set_string("text", item_meta:get_string("text"))
+    block_meta:set_string("infotext", item_meta:get_string("text"))
+    block_meta:set_string("rune_pos", item_meta:get_string("rune_pos"))
+    return block_meta
 end
 
 minetest.register_craft({
@@ -39,12 +61,6 @@ minetest.register_craft({
     recipe = { {"group:pickaxe"}, {"runes:teleport_block_item"}}
 })
 
-minetest.register_craft({
-	type = "cooking",
-	output = "runes:teleport_block_item",
-	recipe = "runes:teleport_gravel_item",
-})
-
 minetest.register_craftitem("runes:teleport_bottle", {
     description = S("Bottle of teleport potion"),
     inventory_image = "runes_teleport_bottle.png",
@@ -52,22 +68,16 @@ minetest.register_craftitem("runes:teleport_bottle", {
     on_use = teleport_rune.create_gravel
 })
 
-minetest.register_craftitem("runes:teleport_gravel_item", {
-    description = S("Gravel with memory"),
-    inventory_image = "runes_teleport_gravel.png",
-    wield_image = "runes_teleport_gravel.png",
-    stack_max=1
-})
-
 minetest.register_node("runes:teleport_gravel", {
     description = S("Gravel with memory"),
     tiles = {"runes_teleport_gravel.png"},
-    groups = {crumbly = 2},
+    groups = {crumbly = 2, not_in_creative_inventory = 1},
     sounds = default.node_sound_gravel_defaults(),
     stack_max=1,
     on_construct = function(pos)
         local meta = minetest.get_meta(pos)
         meta:set_string("formspec", "field[text;;${text}]")
+        teleport_rune.set_block_text(pos, nil)
     end,
     on_receive_fields = function(pos, formname, fields, sender)
         local text = fields.text
@@ -75,27 +85,51 @@ minetest.register_node("runes:teleport_gravel", {
             minetest.chat_send_player(player_name, S("Text too long"))
             return
         end
-        teleport_rune.set_gravel_text(pos, text)
+        teleport_rune.set_block_text(pos, text)
     end,
     after_place_node = function(pos, placer, itemstack, pointed_thing)
-        teleport_rune.set_gravel_text(pos, itemstack:get_meta():get_string("text"))
+        local block_pos = pos
+        local below_pos = {x = pos.x, y = block_pos.y - 1, z = pos.z}
+
+        local gravel_meta = itemstack:get_meta()
+        local below_node = minetest.get_node(below_pos)
+        if below_node.name=="default:stone" then
+            block_pos = below_pos
+            minetest.remove_node(pos)
+            minetest.set_node(below_pos, { name = "runes:teleport_block" } )
+        end
+        local block_meta = minetest.get_meta(block_pos)
+        teleport_rune.restore_block_meta(gravel_meta, block_meta)
     end,
     preserve_metadata = function(pos, oldnode, oldmeta, drops)
-        item_meta = drops[1]:get_meta()
-        item_meta:set_string("text", oldmeta["text"])
-        item_meta:set_string("rune_pos", minetest.serialize(pos))
+        teleport_rune.save_block_meta(pos, "Gravel with memory", oldmeta, drops[1]:get_meta())
     end,
 })
 
-minetest.register_craftitem("runes:teleport_block_item", {
+minetest.register_node("runes:teleport_block", {
     description = S("Stone with memory"),
-    inventory_image = "runes_teleport_block.png",
-    wield_image = "runes_teleport_block.png"
+    tiles = {"runes_teleport_block.png"},
+    groups = {cracky = 3, not_in_creative_inventory = 1},
+    stack_max=1,
+    sounds = default.node_sound_stone_defaults(),
+    after_place_node = function(pos, placer, itemstack, pointed_thing)
+        teleport_rune.restore_block_meta(itemstack:get_meta(), minetest.get_meta(pos))
+    end,
+    preserve_metadata = function(pos, oldnode, oldmeta, drops)
+        teleport_rune.save_block_meta(pos, "Stone with memory", oldmeta, drops[1]:get_meta())
+    end,
 })
 
 minetest.register_node("runes:teleport_block_engraved", {
     description = S("Teleport stone"),
     tiles = {"runes_teleport_engraved.png"},
-    groups = {cracky = 3},
+    groups = {cracky = 3, not_in_creative_inventory = 1},
+    stack_max=1,
     sounds = default.node_sound_stone_defaults(),
+    after_place_node = function(pos, placer, itemstack, pointed_thing)
+        teleport_rune.restore_block_meta(itemstack:get_meta(), minetest.get_meta(pos))
+    end,
+    preserve_metadata = function(pos, oldnode, oldmeta, drops)
+        teleport_rune.save_block_meta(pos, "Teleport stone", oldmeta, drops[1]:get_meta())
+    end,
 })
